45fbf9bf3bae54213630a876a10c9a545fa0234e
Konan Maruyama <zepp25458565@gmail.com>
2020-06-11 19:25:19 +0700

    Add user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    before_action :logged_in_redirect, only: [:create, :new]
 
     def new
     end
@@ -6,14 +7,14 @@ class SessionsController < ApplicationController
 
     def create
 
-        user=User.find_by(email: session_params[:email])
+        user=User.find_by(username: params[:username])
 
-         if user&.authenticate(session_params[:password])
+         if user&.authenticate(params[:password])
             session[:user_id]=user.id
-
+            flash[:success] =  "You have successfully logged in"
             redirect_to root_path
-
-         else 
+         else
+            flash[:error] = "There was something wrong"
             render :new
          end
 
@@ -21,6 +22,19 @@ class SessionsController < ApplicationController
     end
 
 
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "You have successfully logged out"
+        redirect_to login_path
+    end
 
+
+    private 
+      def logged_in_redirect
+        if logged_in?
+            flash[:error] = "You are already logged in"
+            redirect_to root_path
+        end
+      end
 
 end
