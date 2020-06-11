75c49786d320175f521d7c3ea7d6e01295b406df
Konan Maruyama <zepp25458565@gmail.com>
2020-06-11 16:15:17 +0700

    Add create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,26 @@
 class SessionsController < ApplicationController
 
     def new
-        
     end
+
+
+    def create
+
+        user=User.find_by(email: session_params[:email])
+
+         if user&.authenticate(session_params[:password])
+            session[:user_id]=user.id
+
+            redirect_to root_path
+
+         else 
+            render :new
+         end
+
+
+    end
+
+
+
+
 end
